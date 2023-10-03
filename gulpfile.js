9bdcf028581174b292318ff38e640621556931c0
Matheus Brito <120425592+Matheus-Brito22@users.noreply.github.com>
2023-10-03 19:04:16 -0300

    criando o hero e importando a font avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const imagemin = require('gulp-imagemin')
 
 
+
+function images(){
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/css'));
+}
 
 function styles(){
     return gulp.src('./src/styles/*.scss')
@@ -9,13 +16,7 @@ function styles(){
         .pipe(gulp.dest('./dist/css'));
 }
 
-function testeGulp(cb) {
-    console.log ("Olá mundo")
-    cb();
-}
-
-exports.default = styles
-
+exports.default = gulp.parallel(styles, images)
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
