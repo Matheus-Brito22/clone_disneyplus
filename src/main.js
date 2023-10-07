b4c6ace19cd9b82e0ae2b1455dbdc82234d078dd
Matheus Brito <120425592+Matheus-Brito22@users.noreply.github.com>
2023-10-07 12:29:11 -0300

    Ajuste de responsividade

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 document.addEventListener('DOMContentLoaded', function(){
 
     const buttons = document.querySelectorAll('[data-tab-button]');
-   
+
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight
     
+
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if(posicaoAtual < alturaHero){
+           ocultaElemntosHeader();
+        }
+
+        else{
+            exibeElementodHeader();
+        }
+    })
+   
+    //Seção de Atrações, progamação das abas
+
     for (let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(botao){
 
@@ -17,7 +37,22 @@ document.addEventListener('DOMContentLoaded', function(){
             
         })
      }
+
+     for (let i = 0; i <questions.length;i++){
+        questions[i].addEventListener('click', abreOuFechaResposta);
+     }
    })
+
+   //Seção FAQ, progamação accordions
+   function abreOuFechaResposta(elemento){
+
+    const classe = 'faq__questions__item--is-open';
+    console.log(elemento);
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe)
+
+   }
 
    function removeBotaoAtivo(){
     
@@ -35,4 +70,18 @@ document.addEventListener('DOMContentLoaded', function(){
     for (let i = 0; i <tabsContainer.length; i++){
         tabsContainer[i].classList.remove('shows__list--is-active')
     }
+   }
+
+
+   function ocultaElemntosHeader(){
+    const header = document.querySelector('.header');
+    header.classList.add('header--is-hidden');
+
+
+   }
+   function exibeElementodHeader(){
+    const header = document.querySelector('.header');
+    header.classList.remove('header--is-hidden');
+
+
    }
